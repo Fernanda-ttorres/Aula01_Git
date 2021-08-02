269e8e85c98e642fe1eba7a3dacc1603cbb80ca5
Fernanda-ttorres <fernanda.sttorres@gmail.com>
2021-08-02 17:58:30 -0300

    Opção 4

--- Codigo01.cpp
+++ Codigo01.cpp
@@ -9,7 +9,7 @@ int main()
 
   int opcao, n1, n2;
  
-  while ((opcao==1) || (opcao==2) || (opcao==3) );
+  while ((opcao==1) || (opcao==2) || (opcao==3) || (opcao==4) );
   {
   cout << ("Digite o primeiro número: ") << endl;
   cin >> n1; 
@@ -19,6 +19,7 @@ int main()
   cout << ("1.Média entre os números digitados.") << endl;
   cout << ("2.Diferença do maior pelo menor.") << endl;
   cout << ("3.Produto entre os números digitados.") << endl;
+  cout << ("4.Divisão do primeiro pelo segundo.") << endl;
   cin >> opcao;
   
    if (opcao==1)
@@ -36,6 +37,13 @@ int main()
    {
    	cout << ("Produto = ") << (n1*n2);
    } 
+   if (opcao==4)
+   {
+   	if (n2!=0)
+   		cout << ("Divisão = ") << (n1/n2);
+   	else
+	  cout << ("Divisor inválido!"); 
+   }
   }
    
  return 0;
