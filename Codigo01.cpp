697ec2876648e1b28ba6cefb3ed563fa086a9d4b
Fernanda-ttorres <fernanda.sttorres@gmail.com>
2021-08-02 17:56:35 -0300

    Opção 3

--- Codigo01.cpp
+++ Codigo01.cpp
@@ -9,7 +9,7 @@ int main()
 
   int opcao, n1, n2;
  
-  while ((opcao==1) || (opcao==2));
+  while ((opcao==1) || (opcao==2) || (opcao==3) );
   {
   cout << ("Digite o primeiro número: ") << endl;
   cin >> n1; 
@@ -18,6 +18,7 @@ int main()
   cout << ("Escolha uma das seguintes opçãoes:") << endl;
   cout << ("1.Média entre os números digitados.") << endl;
   cout << ("2.Diferença do maior pelo menor.") << endl;
+  cout << ("3.Produto entre os números digitados.") << endl;
   cin >> opcao;
   
    if (opcao==1)
@@ -30,6 +31,10 @@ int main()
    		cout << ("Diferença = ") << (n1-n2);
    	else
 	   	cout << ("Diferença = ") << (n2-n1);
+   }
+    if (opcao==3)
+   {
+   	cout << ("Produto = ") << (n1*n2);
    } 
   }
    
